ded28e888c29d2fb521e61608828edfced75e086
Aldemaro Guzman <aldemaroguzman93@gmail.com>
2018-08-21 08:48:45 -0430

    iniciando clase abstracta, lista clase empleado y jefatura

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ Practicando clase abstractas. de codigo facilito.!
+utilizando clase empleado, jefatura.
+ * @author aldemaro
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
